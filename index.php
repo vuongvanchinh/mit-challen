1fc1e2c91398670638aee17fe2de90e794b40352
Sotatek-ChinhVuong <chinh.vuong@sotatek.com>
2022-01-11 21:04:16 +0700

    initial

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php 
+
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Places </title>
+    <link rel="stylesheet" href="css/index.css">
+    <link rel="stylesheet" href="css/banner.css">
+    <link rel="stylesheet" href="css/footer.css">
+    <link rel="stylesheet" href="css/header.css">
+    <link rel="stylesheet" href="css/places.css">
+
+</head>
+<body>
+    <div class="container">
+        <header class="header">
+            header
+        </header>
+        
+        <div class="banner">
+            banner
+        </div>
+
+        <div class="places">
+            places
+        </div>
+        <footer>
+            footer
+        </footer>
+    </div>
+</body>
+</html>
